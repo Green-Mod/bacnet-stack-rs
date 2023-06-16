725cf093c665189b2179d24f077bce28e03ba005
Giorgio Bertolotti <giorgiobertol@gmail.com>
2023-06-16 12:01:23 +0200

    :recycle: Make I Am example work with broadcast and without direct communication with the whois client

--- bacnet-sys/examples/iam.rs
+++ bacnet-sys/examples/iam.rs
@@ -1,75 +1,32 @@
+use bacnet_sys::*;
 use std::ffi::CStr;
 
-use bacnet_sys::*;
-
-static mut SHOULD_SEND_I_AM: bool = false;
-
-const TARGET_IP_ADDRESS: &str = "192.168.200.101:47809";
-const LOCAL_PORT: u16 = 47810;
 const INSTANCE_NUMBER: u32 = 1234;
-const DNET: u16 = 0 as u16;
 
 fn main() {
     unsafe {
-        address_init();
-        let mut dest: BACNET_ADDRESS = BACNET_ADDRESS {
-            adr: [0; 7],
-            mac_len: 0,
-            len: 0,
-            mac: [0; 7],
-            net: 0,
-        };
+        println!(
+            "bacnet stack v{}",
+            String::from_utf8_lossy(BACNET_VERSION_TEXT)
+        );
 
-        let mut adr: BACNET_MAC_ADDRESS = BACNET_MAC_ADDRESS {
-            adr: [0; 7],
-            len: 0,
-        };
-        bacnet_address_mac_from_ascii(&mut adr, TARGET_IP_ADDRESS.as_ptr() as *const i8);
+        let mut dest = BACNET_ADDRESS::default();
 
-        let mut mac: BACNET_MAC_ADDRESS = BACNET_MAC_ADDRESS {
-            adr: [0; 7],
-            len: 0,
-        };
-        bacnet_address_mac_from_ascii(&mut mac, TARGET_IP_ADDRESS.as_ptr() as *const i8);
+        let a = BACNET_BROADCAST_NETWORK;
+        println!("BACNET_BROADCAST_NETWORK={}", a);
 
-        bacnet_address_init(&mut dest, &mut mac, DNET, &mut adr);
-
-        dest.adr = adr.adr;
-        dest.len = adr.len;
-        dest.net = DNET;
-
-        // bip_get_broadcast_address(&mut dest);
-
-        println!("{:?}", dest);
+        bip_get_broadcast_address(&mut dest as *mut _);
 
         Device_Set_Object_Instance_Number(INSTANCE_NUMBER);
         println!("BACnet Device ID: {}", Device_Object_Instance_Number());
 
-        Device_Init(&mut object_functions {
-            Object_Type: BACNET_OBJECT_TYPE::BITS,
-            Object_Init: None,
-            Object_Count: None,
-            Object_Index_To_Instance: None,
-            Object_Valid_Instance: None,
-            Object_Name: None,
-            Object_Read_Property: None,
-            Object_Write_Property: None,
-            Object_RPM_List: None,
-            Object_RR_Info: None,
-            Object_Iterator: None,
-            Object_Value_List: None,
-            Object_COV: None,
-            Object_COV_Clear: None,
-            Object_Intrinsic_Reporting: None,
-            Object_Add_List_Element: None,
-            Object_Remove_List_Element: None,
-        });
+        Device_Init(&mut object_functions::default());
 
         /* we need to handle who-is
         to support dynamic device binding to us */
         apdu_set_unconfirmed_handler(
             BACnet_Unconfirmed_Service_Choice_SERVICE_UNCONFIRMED_WHO_IS,
-            Some(my_handler_who_is),
+            Some(handler_who_is),
         );
         /* set the handler for all the services we don't implement
         It is required to send the proper reject message... */
@@ -88,64 +45,29 @@ fn main() {
         apdu_set_abort_handler(Some(my_abort_handler));
         apdu_set_reject_handler(Some(my_reject_handler));
 
-        bip_set_port(LOCAL_PORT);
-        println!("Running on port {}", bip_get_port());
         address_init();
         dlenv_init();
 
         // Broadcasted I Am request only works for services on the same port
-        // Send_I_Am(&mut Handler_Transmit_Buffer[0]);
-
-        Send_I_Am_To_Network(
-            &mut dest,
-            Device_Object_Instance_Number(),
-            MAX_APDU,
-            BACNET_SEGMENTATION_SEGMENTATION_NONE as i32,
-            BACNET_VENDOR_ID as u16,
-        );
+        Send_I_Am(&mut Handler_Transmit_Buffer[0]);
 
         let mut rx_buf: [u8; MAX_MPDU as usize] = [0; MAX_MPDU as usize];
 
-        let mut src: BACNET_ADDRESS = BACNET_ADDRESS {
-            adr: [0; 7],
-            mac_len: 0,
-            len: 0,
-            mac: [0; 7],
-            net: 0,
-        };
+        let mut src: BACNET_ADDRESS = BACNET_ADDRESS::default();
 
         loop {
             let pdu_len = bip_receive(&mut src, &mut rx_buf[0], MAX_MPDU as u16, 1000);
 
             if pdu_len > 0 {
+                println!(
+                    "Received PDU of {} bytes: {:?}",
+                    pdu_len,
+                    &rx_buf[..pdu_len as usize]
+                );
                 // apdu_handler(&mut src, &mut rx_buf[0], pdu_len);
                 npdu_handler(&mut src, &mut rx_buf[0], pdu_len);
             }
-
-            if SHOULD_SEND_I_AM {
-                SHOULD_SEND_I_AM = false;
-                Send_I_Am_To_Network(
-                    &mut src,
-                    Device_Object_Instance_Number(),
-                    MAX_APDU,
-                    BACNET_SEGMENTATION_SEGMENTATION_NONE as i32,
-                    BACNET_VENDOR_ID as u16,
-                );
-            }
         }
-    }
-}
-
-#[no_mangle]
-extern "C" fn my_handler_who_is(
-    service_request: *mut u8,
-    service_len: u16,
-    src: *mut BACNET_ADDRESS,
-) {
-    unsafe {
-        handler_who_is(service_request, service_len, src);
-
-        SHOULD_SEND_I_AM = true;
     }
 }
 
